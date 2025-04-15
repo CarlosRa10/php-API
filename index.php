0f579393fb5f8ff003b319523c98cd54d64df0cc
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-15 17:25:59 -0400

    Agrega lógica para obtener y mostrar datos de la API de películas de Marvel

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
-
+const API_URL = "https://whenisthenextmcufilm.com/api";
+//Inicializar una nueva sesión de CURL; ch = Curl handle
+//Indicar que queremmos recibir el resultado de la petición y no mostrarla en pantallacurl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);// para que me devuelva el resultado
+//Ejecutar la petición y guardar el resultado en una variable
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+curl_close($ch);
+var_dump($data);
 ?>
 
-<h2></h2>
+
+
+<main>
+    <h2>La próxima película de Marvel</h2>
+</main>
+
+
+
 
 <style>
   :root {
