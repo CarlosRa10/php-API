93baaa5c429f7580ac315af6fc3226be29a418e9
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-15 10:08:52 -0400

    Reemplaza el mensaje de saludo por variables y estilos en index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-echo "Hola, Mundo!";
+    $nombre = "Carlos";
+    $edad = 25;
+    $numero_favorito = (bool) 7;// 7 es verdadero, 0 es falso - casting a booleano
+    $frase_favorita = "La vida es bella";
+    var_dump($nombre, $edad, $frase_favorita);
 ?>
+
+
+<h1>
+    <?= "Hola ". $nombre?>
+</h1>
+
+
+<style>
+
+:root {
+    color-scheme: light dark;
+}
+
+body {
+display: grid;
+place-content: center;
+}
+
+</style>
